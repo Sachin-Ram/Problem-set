a584b9fc3e330a38eb679171286f5221d6b387e9
Sachin Ram <sachinram2510@gmail.com>
2024-02-15 23:13:51 +0530

    files added

---
+++ pattern.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class pattern {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the Number of Rows : ");
+        int N=input.nextInt();
+
+        for (int row = 1; row <= N; row++) 
+        {
+            for (int col = 1; col <= N + row - 1; col++) 
+            {
+                if(col <= N-row) {
+                    System.out.print(" ");
+                }
+
+                else{
+
+                System.out.print("+");}
+            }
+            System.out.println();
+        }
+        input.close();
+    }
+}
